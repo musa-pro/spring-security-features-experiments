942d4924249eca05797c3205a26f07f97637de94
Mohammad Abu Musa <MohammadAbu.Musa@thomsonreuters.com>
2025-01-29 16:33:23 -0500

    abled to get authorized code using form login, then got accesstoken using rest client manually(not OAuth2 restclient)

--- oauth2-social-login-example/client-application/src/main/java/com/example/client_application/ClientController.java
+++ oauth2-social-login-example/client-application/src/main/java/com/example/client_application/ClientController.java
@@ -2,7 +2,13 @@ package com.example.client_application;
 
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +17,8 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 import java.util.UUID;
+
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
 @RestController
 public class ClientController {
@@ -36,42 +44,53 @@ public class ClientController {
                 .queryParam("response_type", "code")
                 .queryParam("client_id", "public-client")
                 .queryParam("redirect_uri", "http://localhost:3000/callback")
-                .queryParam("scope", "profile openid")
-//                .queryParam("state", UUID.randomUUID().toString().replace("-", ""))
-                .queryParam("code_challenge", codeChallenge)
-                .queryParam("code_challenge_method", "S256")
+                .queryParam("scope", "profile openid read write")
+                .queryParam("state", UUID.randomUUID().toString().replace("-", ""))
+//                .queryParam("code_challenge", codeChallenge)
+//                .queryParam("code_challenge_method", "S256")
                 .build()
                 .toUriString();
         response.sendRedirect(authorizationUrl);
     }
 
     @GetMapping("/callback")
-    public void callback(@RequestParam(value = "code", required = false) String code, @RequestParam(value = "state", required = false) String state,
-                           @RequestParam(value = "error", required = false) String error,
-                           @RequestParam(value = "error_description", required = false) String error_description){
+    public ResponseEntity<String> callback(@RequestParam(value = "code", required = false) String authorizationCode, @RequestParam(value = "state", required = false) String state,
+                                                      @RequestParam(value = "error", required = false) String error,
+                                                      @RequestParam(value = "error_description", required = false) String error_description){
 
 
-        if (code != null) {
+        if (authorizationCode != null) {
             HttpHeaders headers = new HttpHeaders();
-//            headers.setBasicAuth(clientId, clientSecret);
-//            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-//
-//            MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-//            body.add("grant_type", "authorization_code");
-//            body.add("code", authorizationCode);
-//            body.add("redirect_uri", redirectUri);
-//
-//            OAuth2AccessToken accessToken = restClient.post()
-//                    .uri("http://localhost:8080/oauth2/token")
-//                    .headers(headers)
-//                    .body(BodyInserters.fromValue(body))
-//                    .retrieve()
-//                    .bodyToMono(OAuth2AccessToken.class)
-//                    .block();
-            System.out.println("code: " + code);
+            headers.setBasicAuth("public-client", "secret");
+            headers.setContentType(MediaType.valueOf(MULTIPART_FORM_DATA_VALUE));
+
+
+            MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+            formData.add("grant_type", "authorization_code");
+            formData.add("code", authorizationCode);
+            formData.add("redirect_uri", "http://localhost:3000/callback");
+//            body.add("state", state);
+
+            try{
+                ResponseEntity<String> response = restClient.post()
+                        .uri("http://localhost:8080/oauth2/token")
+                        .headers(h -> {
+                            h.addAll(headers);
+                        })
+                        .body(formData)
+                        .retrieve()
+                        .toEntity(String.class);
+                return ResponseEntity.ok(response.getBody());
+
+            }catch (Exception e){
+                e.printStackTrace();
+                return ResponseEntity.ok(null);
+            }
+
         } else {
             System.out.println("error: " + error);
             System.out.println("error_description: " + error_description);
+            return ResponseEntity.ok(null);
         }
     }
 }
