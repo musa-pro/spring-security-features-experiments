b23ff6f0ef1a181ab852300d0175f3299eff4ba4
Mohammad Abu Musa <MohammadAbu.Musa@thomsonreuters.com>
2025-01-29 15:24:21 -0500

    a client added which ables to get authorization code using pkce

---
+++ oauth2-social-login-example/authorization-server/src/main/java/com/example/authorization_server/AuthorizationServerOIDCVersionApplication.java
@@ -0,0 +1,13 @@
+package com.example.authorization_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServerOIDCVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServerOIDCVersionApplication.class, args);
+	}
+
+}
